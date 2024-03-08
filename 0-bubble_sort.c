f3087d2e6615a5bd355aa32c23273c1a76f9ab89
ck-ngetich <kiprongetich3@gmail.com>
2024-03-07 17:55:08 -0800

    its a bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,46 @@
 #include "sort.h"
+#include <stdbool.h>
 /**
- * bubble_sort - function that sorts elements of array in an asceding order
- * @array: array with elements
- * @size: size of an array
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, len = size;
+	bool bubbly = false;
 
-	size_t i, x, tmp = 0;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (x = 0; x < size; x++)
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[x] > array[x + 1] && array[x + 1])
+			if (array[i] > array[i + 1])
 			{
-			tmp = array[x];
-			array[x] = array[x + 1];
-			array[x + 1] = tmp;
-			print_array(array, size);
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
+		len--;
+	}
 }
